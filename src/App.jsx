76653313b26289a3ec7abee44ee64655eb4f4a38
renatojp15 <renatoj.p@outlook.com>
2025-07-30 16:37:51 -0500

    Agregando primer parrafo para la etapa 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import WordDictation from './components/wordDictation'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">
-          Hello, Word Dictation!
-        </h1>
-        <p className="text-lg text-gray-700">
-          Tailwind CSS está funcionando correctamente 💪
-        </p>
-        <button className="mt-6 px-6 py-2 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-lg shadow">
-          ¡Empezar!
-        </button>
-      </div>
+      <main className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <WordDictation />
+      </main>
     </>
   )
 }
---
+++ src/data/lesson1.js
@@ -0,0 +1,6 @@
+const lesson1 = {
+  paragraph: "The quick brown fox jumps over the lazy dog.",
+  words: ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"],
+};
+
+export default lesson1;
